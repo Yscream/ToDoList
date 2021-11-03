e79f806d0b4b2eb72b52701d663dcece28ed111b
Egor Kopyl <kopyl.egor1@gmail.com>
2021-11-03 16:20:50 +0300

    fixed2

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    plugins: ['import'],
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+      "no-shadow": ["error", { "builtinGlobals": false, "hoist": "never", "allow": [] }],
+    },
+    
+  };
--- src/index.js
+++ src/index.js
@@ -1,74 +1,73 @@
-window.addEventListener('load', ready);
+const arr = []
 
-let arr = [];
+function createLi() {
+    const li = document.createElement('li')
+    const ul = document.querySelector('#ul')
+    for (let i = 0; i < arr.length; i++) {
+        li.innerHTML = arr[i]
+        ul.append(li)
+    }
+    const arr2 = []
+    // Add all list to a new array
+    for (let i = 0; i < ul.childNodes.length; i++) {
+        arr2.push(ul.childNodes[i].innerHTML)
+    }
+    // Sort the list
+    arr2.sort()
+    ul.innerHTML = '' // если убрать этот параметр то при каждом новом добавлении элемента в список,
 
-function ready(){
+    // add all arr to a new list
+    for (let j = 0; j < arr2.length; j++) {
+        const li = document.createElement('li')
+        li.innerHTML = arr2[j]
+        ul.append(li)
+    }
+}
 
-    let input = document.createElement('input');
-    input.classList.add('input');
-    document.body.append(input);
+function add() {
+    if (document.querySelector('.input').value.trim().length === 0) {
+        return
+    }
+    arr.push(document.querySelector('.input').value)
+    createLi()
+    const value = JSON.stringify(arr)
+    localStorage.setItem('task', value)
+    document.querySelector('.input').value = ''
+}
 
-    let button = document.createElement('button');
-    button.textContent = "Add";
+function ready() {
+    const input = document.createElement('input')
+    input.classList.add('input')
+    document.body.append(input)
+
+    const button = document.createElement('button')
+    button.textContent = 'Add'
     button.addEventListener('click', add)
-    document.body.append(button);
+    document.body.append(button)
 
-    let div = document.createElement('div');
-    div.id = 'container';
-    document.body.append(div);
+    const div = document.createElement('div')
+    div.id = 'container'
+    document.body.append(div)
 
-    let ul = document.createElement('ul');
-    ul.id = 'ul';
-    document.querySelector('#container').append(ul);
+    const ul = document.createElement('ul')
+    ul.id = 'ul'
+    document.querySelector('#container').append(ul)
 
-    document.querySelector('.input').onkeydown = function(event){
+    document.querySelector('.input').onkeydown = function key(event) {
         if (event.keyCode === 13) {
-            add();
+            add()
         }
     }
-    if(!localStorage.getItem('task')){
-        return arr;
-    }else{
-        const value = JSON.parse(localStorage.getItem('task'));
-        for(let i = 0; i < value.length; i++){
-            const li = document.createElement('li');
-            li.innerHTML = value[i];
-            document.querySelector('#ul').append(li);
-        }
+    if (!localStorage.getItem('task')) {
+        return arr
     }
+    const value = JSON.parse(localStorage.getItem('task'))
+    for (let i = 0; i < value.length; i++) {
+        const li = document.createElement('li')
+        li.innerHTML = value[i]
+        document.querySelector('#ul').append(li)
+    }
+    return ''
 }
 
-function createLi(){
-    const li = document.createElement('li'); 
-    const ul = document.querySelector('#ul');
-        for(let i = 0; i < arr.length; i++) {
-            li.innerHTML = arr[i];
-            ul.append(li);
-        }  
-        let arr2 = [];
-        // Add all list to a new array
-        for (let i = 0; i < ul.childNodes.length; i++) {
-            arr2.push(ul.childNodes[i].innerHTML);
-        }
-        // Sort the list
-        arr2.sort();
-        ul.innerHTML = '';//если убрать этот параметр то при каждом новом добавлении элемента в список,
-        
-        //add all arr to a new list
-        for (let j = 0; j < arr2.length; j++) {
-            let li = document.createElement('li');
-            li.innerHTML = arr2[j];
-            ul.append(li);
-        }
-}
-
-function add(){ 
-        if(document.querySelector('.input').value.trim().length === 0){
-            return;
-        }
-        arr.push(document.querySelector('.input').value);
-        createLi();
-        let value = JSON.stringify(arr);
-        localStorage.setItem('task', value);
-        document.querySelector('.input').value = '';
-}
+window.addEventListener('load', ready)
